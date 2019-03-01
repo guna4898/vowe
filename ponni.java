38184ba04d5765871242a244eb9a3530daf5d056
guna4898 <47376893+guna4898@users.noreply.github.com>
2019-03-01 05:14:29 -0800

    Create ponni.java

---
+++ ponni.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	char ch= ' ';
+	Scanner s=new Scanner(System.in);
+	ch=s.next().charAt(0);
+	int n=(int)ch;
+	
+	if(n>=65 && n<=123)
+	
+	
+		System.out.println("Alphabet");
+	
+	else
+	
+		System.out.println("no");
+	
+}
+}
